5364edb075c4e8c1e219d0de3905470c62098c4b
KINGODREVA <129159391+KINGODREVA@users.noreply.github.com>
2023-04-19 14:23:57 +0900

    Update Timer_LED.ino

--- Timer_LED/Timer_LED.ino
+++ Timer_LED/Timer_LED.ino
@@ -1,3 +1,5 @@
+/*
+
 #include <TimerOne.h>
 
 const int LED = 10;
@@ -17,5 +19,24 @@ void loop() {
   {
     Timer1.setPwmDuty(LED, t_high*100);
     delay(100);
+  }
+}
+
+*/
+
+unsigned int x;
+
+int main(void){
+  
+  Serial.begin(9600);
+  TCCR1A = 0b00000000;
+  TCCR1B = 0b00000101;
+
+  while(1){
+    //Serial.println(TCNT1);
+    _delay_ms(100);
+    x = x+100;
+    Serial.println(x);
+    
   }
 }
